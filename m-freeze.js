3088dde0f731da121b69c5061a7dfb13465549f5
necrongg <necron7577@daum.net>
2026-01-27 16:02:17 +0900

    중계수정

--- m-freeze.js
+++ m-freeze.js
@@ -109,7 +109,7 @@ function startFreezeObserver() {
                 createButton(container, '이마트', '이마트', 'white', 'green', () => {
                     setElementsValues({
                         STRR_ID: '',
-                        ITEM_GCD: 'A004,A013,A039,A42,A045,A007,A047,A012,A042',
+                        ITEM_GCD: 'A004,A013,A039,A42,A045,A007,A047,A012,A042,A028,A043,A046',
                         CUST_CD: '',
                         SHIPTO_ID: '',
                         SHIPTO_TCD: '20',
